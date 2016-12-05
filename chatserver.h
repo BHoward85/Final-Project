2ed63946c8ad253a80f01c3b6e351c3846887069
rwt88 <RodneyThomas88@gmail.com>
2016-12-05 11:46:52 -0800

    Final Update

--- chatserver.h
+++ chatserver.h
@@ -1,5 +1,5 @@
-// Final Project
-// Server Header
+// Final Project - CSCD 330
+// Server - Written by Rodney Thomas
 
 #ifndef CHATSERVER_H
 #define CHATSERVER_H
@@ -13,11 +13,14 @@
 #include <sys/types.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
+// Brads files
+#include "packetmanager.h"
+#include "compar.h"
 
 #define SERVER_SIZE 16 // max number of users
-#define UNAME_MAX 15 // who needs a 15 character uname anyway?
-#define MAX 50 // packet size
-#define MAX_MESS 42 // max message size
+#define UNAME_MAX 15 // max uname allowed
+#define MAX 50 // constant packet size
+#define MAX_MESS 42 // max message size within packet
 
 typedef struct Client
 {
@@ -30,16 +33,16 @@ typedef struct Client
 // login get username -> give id
 void login(int conn_fd, int max_fd, int id);
 
+// delegate what to do with packet (logout, sendPacket, commandPacket)
+void readPacket(fd_set *rfd, char packet[MAX], int max_fd, int j);
+
 // simple logout
 void logout(fd_set *rfd, char chan, int source, int max_fd, int j);
 
-// delegate what to do with packet
-void readPacket(fd_set *rfd, char packet[MAX], int max_fd, int j);
-
-// send packet to channel
+// send packet to current channel users
 void sendPacket(char chan, int source, char data[MAX_MESS], int max_fd, int j);
 
-// use comPar to figure out what client wants to do and then do it
-void commandPacket(char packet[MAX], int source, char chan, int max_fd, int j);
+// use comPar to figure out what client wants to do and then do it ($ch, $pm, $ul)
+void commandPacket(char packet[MAX], int source, char chan, char msg[MAX_MESS], int max_fd, int j);
 
 #endif
